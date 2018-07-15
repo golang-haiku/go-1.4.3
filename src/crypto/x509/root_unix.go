92a999a9da007edb6e50667ebf9590ba3c1c0446
Calvin Hill <calvin@hakobaito.co.uk>
2018-07-15 04:06:58 +0100

    crypto: Haiku support.

--- src/crypto/x509/root_unix.go
+++ src/crypto/x509/root_unix.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build dragonfly freebsd linux nacl netbsd openbsd solaris
+// +build dragonfly freebsd haiku linux nacl netbsd openbsd solaris
 
 package x509
 
@@ -10,13 +10,14 @@ import "io/ioutil"
 
 // Possible certificate files; stop after finding one.
 var certFiles = []string{
-	"/etc/ssl/certs/ca-certificates.crt",     // Debian/Ubuntu/Gentoo etc.
-	"/etc/pki/tls/certs/ca-bundle.crt",       // Fedora/RHEL
-	"/etc/ssl/ca-bundle.pem",                 // OpenSUSE
-	"/etc/ssl/cert.pem",                      // OpenBSD
-	"/usr/local/share/certs/ca-root-nss.crt", // FreeBSD/DragonFly
-	"/etc/pki/tls/cacert.pem",                // OpenELEC
-	"/etc/certs/ca-certificates.crt",         // Solaris 11.2+
+	"/etc/ssl/certs/ca-certificates.crt",           // Debian/Ubuntu/Gentoo etc.
+	"/etc/pki/tls/certs/ca-bundle.crt",             // Fedora/RHEL
+	"/etc/ssl/ca-bundle.pem",                       // OpenSUSE
+	"/etc/ssl/cert.pem",                            // OpenBSD
+	"/usr/local/share/certs/ca-root-nss.crt",       // FreeBSD/DragonFly
+	"/etc/pki/tls/cacert.pem",                      // OpenELEC
+	"/etc/certs/ca-certificates.crt",               // Solaris 11.2+
+	"/boot/system/data/ssl/CARootCertificates.pem", // Haiku
 }
 
 // Possible directories with certificate files; stop after successfully
